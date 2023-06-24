2631658f2e9ce53b1d341081c85480066f241fae
Bohaniqa <bohaniqa@gmail.com>
2023-06-24 12:40:50 +0100

    Free mint

--- pages/mint.tsx
+++ pages/mint.tsx
@@ -96,7 +96,7 @@ console.log('Shift Program ID', shiftProgramId);
 
 const candyMachinePubkey = umiPublicKey(candyMachineId);
 let umi = createUmi(network).use(mplCandyMachine());
-const solPrice = 5.95;
+const solPrice = 0.7;
 let purchased = 0;
 
 export default function Mint() {
