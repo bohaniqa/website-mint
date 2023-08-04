943861efcb56847b5e9535d403f1f51e49b15dd4
Bohaniqa <bohaniqa@gmail.com>
2023-08-04 23:07:51 +0100

    Update title

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     return (
     <>
         <Head>
-        <title>BOHANIQA Mint</title>
+        <title>Bohaniqa - Mint</title>
         <meta name="description" content="10K digital mining machines." />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
